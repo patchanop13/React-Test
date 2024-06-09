548906906811e93b950e396ca8b013ec1deb6535
patchanopjob <patchanop.l@codemonday.com>
2024-06-09 15:47:39 +0700

    fix: change imported path

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
 import "./index.css";
 import "./i18n.ts";
 import "./theme.scss";
 import { Provider } from "react-redux";
-import { store } from "./store/store.ts";
 import dayjs from "dayjs";
 import advancedFormat from "dayjs/plugin/advancedFormat";
 import customParseFormat from "dayjs/plugin/customParseFormat";
@@ -13,6 +11,8 @@ import localeData from "dayjs/plugin/localeData";
 import weekday from "dayjs/plugin/weekday";
 import weekOfYear from "dayjs/plugin/weekOfYear";
 import weekYear from "dayjs/plugin/weekYear";
+import { store } from "./store/store";
+import App from "./App";
 
 dayjs.extend(customParseFormat);
 dayjs.extend(advancedFormat);
